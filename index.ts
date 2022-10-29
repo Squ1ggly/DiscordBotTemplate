3f73ec0c11f4b04c025c2df9de9ea2668d6da07e
Squ1ggly <squ1gglyzerocool@gmail.com>
2022-10-29 20:23:21 +1000

    Added chalk

--- index.ts
+++ index.ts
@@ -5,41 +5,49 @@ import { updateDiscordCommands } from "./src/syncCommands";
 import codes from "./botCodes.json";
 import { IClientWitPrefixCommands, setPrefixCommands } from "./src/setPrefixCommands";
 import coolDown from "./src/coolDown";
+import chalk from "chalk";
 
-//cmd prefix not used for this bot
-const commandPrefix = "sq!";
+try {
+  if (codes.botId && codes.token) {
+    //cmd prefix not used for this bot
+    const commandPrefix = "sq!";
 
-const coolDowns = new Map();
+    const coolDowns = new Map();
 
-const client: IClientWitCommands & IClientWitPrefixCommands = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
-});
+    const client: IClientWitCommands & IClientWitPrefixCommands = new Client({
+      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+    });
 
-setCommands(client);
-setPrefixCommands(client);
-client.on("ready", async () => {
-  console.log(`Bot Started`);
-  await updateDiscordCommands(client.commandsArray);
-});
+    setCommands(client);
+    setPrefixCommands(client);
+    client.on("ready", async () => {
+      console.log(`Bot Started`);
+      await updateDiscordCommands(client.commandsArray);
+    });
 
-client.on("interactionCreate", async (interaction: Interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  if (!checkCommand(client.commandsArray, interaction.commandName)) return;
-  if (!coolDown(interaction, coolDowns, interaction.commandName)) {
-    await client.commands.get(interaction.commandName).execute(interaction, client);
-  }
-});
+    client.on("interactionCreate", async (interaction: Interaction) => {
+      if (!interaction.isChatInputCommand()) return;
+      if (!checkCommand(client.commandsArray, interaction.commandName)) return;
+      if (!coolDown(interaction, coolDowns, interaction.commandName)) {
+        await client.commands.get(interaction.commandName).execute(interaction, client);
+      }
+    });
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-  if (!message.content) return;
-  const command = message.content.toLowerCase();
-  if (!command.toLowerCase().startsWith(commandPrefix)) return;
-  const commandText = command.toLocaleLowerCase().replace(commandPrefix, "").split(" ")[0];
-  if (!checkCommand(client.prefixCommandsArray, commandText)) return;
-  if (!coolDown(message, coolDowns, commandText)) {
-    await client.prefixCommands.get(commandText).execute(message, client);
-  }
-});
+    client.on("messageCreate", async (message) => {
+      if (message.author.bot) return;
+      if (!message.content) return;
+      const command = message.content.toLowerCase();
+      if (!command.toLowerCase().startsWith(commandPrefix)) return;
+      const commandText = command.toLocaleLowerCase().replace(commandPrefix, "").split(" ")[0];
+      if (!checkCommand(client.prefixCommandsArray, commandText)) return;
+      if (!coolDown(message, coolDowns, commandText)) {
+        await client.prefixCommands.get(commandText).execute(message, client);
+      }
+    });
 
-client.login(codes.token);
+    client.login(codes.token);
+
+  } else console.log(chalk.redBright("\n        WARNING : Please put credentials for bot in botCodes.json\n"));
+} catch (err) {
+  console.log(`${err}`)
+}
