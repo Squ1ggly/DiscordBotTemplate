807424b79106baa9267bfad0534ae080552ac465
Squ1ggly <squ1gglyzerocool@gmail.com>
2022-11-13 18:35:32 +1000

    FEAT: Added command for cleaning guild commands from the bot, FIX: updated packages and added yarn, CHOR: removed .vscode from project

--- src/syncCommands.ts
+++ src/syncCommands.ts
@@ -1,20 +1,39 @@
 import { Routes } from "discord.js";
 import codes from "../botCodes.json";
 import { REST } from "@discordjs/rest";
+import { ICommand } from "./setCommands";
 
-const rest = new REST({ version: "10" }).setToken(codes.token);
-
-export async function updateDiscordCommands(commands) {
+export async function updateDiscordCommands(commands: ICommand[]) {
   try {
+    const rest = new REST({ version: "10" }).setToken(codes.token);
 
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands(codes.botId), { body: [] })
+    await rest.put(Routes.applicationCommands(codes.botId), { body: [] });
 
     await rest.put(Routes.applicationCommands(codes.botId), { body: commands });
+
+    console.log("Getting Current (/) Commands");
+
+    console.log((await rest.get(`/applications/${codes.botId}/commands`)));
 
     console.log("Successfully reloaded application (/) commands.");
   } catch (err) {
     console.error(err);
+  }
+}
+
+export async function updateGuildCommands(serverId: string) {
+  try {
+    const rest = new REST({ version: "10" }).setToken(codes.token);
+
+    console.log("Started refreshing application guild (/) commands.");
+
+    await rest.put(Routes.applicationGuildCommands(codes.botId, serverId), { body: [] });
+
+    console.log("Successfully reloaded application guild (/) commands.");
+  } catch (err) {
+    console.error(err);
+    throw err
   }
 }
