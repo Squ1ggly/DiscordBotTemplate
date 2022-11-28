ea01b91e752c5e32f236797cf8754b604c93d05c
Squ1ggly <squ1gglyzerocool@gmail.com>
2022-11-29 00:10:02 +1000

    FIX: formatted genenv.js

--- genenv.js
+++ genenv.js
@@ -1,8 +1,10 @@
 const { writeFileSync, readdirSync } = require("fs");
 const checkEnv = readdirSync(".").filter((e) => e === ".env" || e === "REMOVE_THIS_TEXT.env");
-if (checkEnv.length < 1)
-  return writeFileSync(
+if (checkEnv.length < 1) {
+  writeFileSync(
     "./REMOVE_THIS_TEXT.env",
     `token=YOUR_TOKEN_HERE\nbotId=BOT_APPLICATION_ID\nbotImage=BOT_IMAGE_URL\nserverId=DISCORD_SERVER_ID`
   );
+  return;
+}
 console.log(".env already exists");
