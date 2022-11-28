7d1a970508caeb218f6cd14d3959cf153a27808a
Squ1ggly <squ1gglyzerocool@gmail.com>
2022-11-29 01:41:33 +1000

    CHOR: Formated file

--- copyfilestoenv.js
+++ copyfilestoenv.js
@@ -4,4 +4,4 @@ const packageJson = JSON.parse(readFileSync("./package.json"));
 delete packageJson.scripts.genenv;
 packageJson.scripts.start = "npm i && node ./src/index.js";
 writeFileSync("./dist/.env", env);
-writeFileSync("./dist/package.json", JSON.stringify(packageJson,null,2));
+writeFileSync("./dist/package.json", JSON.stringify(packageJson, null, 2));
