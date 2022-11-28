1db1029034537288ae795062de1eead57524a0e5
Squ1ggly <squ1gglyzerocool@gmail.com>
2022-11-29 00:02:53 +1000

    FIX : Bug in the genenv.js would always fail to generate .env

--- genenv.js
+++ genenv.js
@@ -1,6 +1,6 @@
 const { writeFileSync, readdirSync } = require("fs");
-const checkEnv = readdirSync("./src/SlashCommands").filter((e) => e === ".env" || e === "REMOVE_THIS_TEXT.env");
-if (!checkEnv)
+const checkEnv = readdirSync(".").filter((e) => e === ".env" || e === "REMOVE_THIS_TEXT.env");
+if (checkEnv.length < 1)
   return writeFileSync(
     "./REMOVE_THIS_TEXT.env",
     `token=YOUR_TOKEN_HERE\nbotId=BOT_APPLICATION_ID\nbotImage=BOT_IMAGE_URL\nserverId=DISCORD_SERVER_ID`
